2aec9b0c66e914ae7464bce1e6966c6c60cc2a09
fabiobonina <fabiobonina@gmail.com>
2016-05-25 21:15:43 -0300

    instalação do Migrations

---
+++ ProjetoRiskZones/ProjetoRiskZones.Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -0,0 +1,22 @@
+﻿using ProjetoRiskZones.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoRiskZones.Infra.Data.Contexto
+{
+    class ProjetoModeloContext : DbContext
+    {
+
+        public ProjetoModeloContext()
+            : base ("ProjetoRiskZones")
+        {
+
+        }
+        //BdSet: Comando pra criar a tabela da class
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
