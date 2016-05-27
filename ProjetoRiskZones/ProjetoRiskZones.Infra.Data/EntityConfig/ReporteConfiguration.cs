9970f1cb9d921b697dc55595901b9d8cd9321104
fabiobonina <fabiobonina@gmail.com>
2016-05-27 10:44:16 -0300

    Correção ReporteConfiguration

--- ProjetoRiskZones/ProjetoRiskZones.Infra.Data/EntityConfig/ReporteConfiguration.cs
+++ ProjetoRiskZones/ProjetoRiskZones.Infra.Data/EntityConfig/ReporteConfiguration.cs
@@ -12,16 +12,20 @@ namespace ProjetoRiskZones.Infra.Data.EntityConfig
             Property(c => c.Status)
                 .IsRequired();
 
-            Property(c => c.IDUsuario)
-                .IsRequired();
-            Property(c => c.IDSintoma)
-                .IsRequired();
-
-            Property(c => c.IDRisco)
-                .IsRequired();
-
             Property(c => c.DataCheckin)
                 .IsRequired();
+
+            HasRequired(c => c.Usuario)
+                .WithMany()
+                .HasForeignKey(c => c.IDUsuario);
+
+            HasRequired(c => c.Sitoma)
+                .WithMany()
+                .HasForeignKey(c => c.IDSintoma);
+
+            HasRequired(c => c.Risco)
+                .WithMany()
+                .HasForeignKey(c => c.IDRisco);
 
         }
     }
